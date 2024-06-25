ce792d0d9f7e17a29ec134b9e1542a898b46c2f8
Tristan Kreuziger <github@tkreuziger.com>
2024-06-25 12:59:37 +0200

    Added caching.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import caldav
+import datetime as dt
 
 from utils import (
     setup_logging,
@@ -10,9 +11,10 @@ from utils import (
     force_timestamp,
 )
 from events import Event
+from cache import check_cache_dir, read_cache_file, write_cache_file
 
 
-def process_calendar(calendar: dict) -> None:
+def process_calendar(calendar: dict, delete_remote_events: bool = False) -> None:
     server_url = calendar['url']
     calendar_id = calendar['id']
     calendar_url = server_url + calendar_id
@@ -29,23 +31,57 @@ def process_calendar(calendar: dict) -> None:
         key=lambda event: force_timestamp(event.scheduled),
     )
 
+    cache_filename = f'{calendar_id}.yaml'
+    cache_file = read_cache_file(cache_filename)
+
+    if not cache_file:
+        write_cache_file(cache_filename, {'cache': {}})
+        old_cache = {}
+    else:
+        old_cache = cache_file.get('cache', {})
+
+    old_events = []
+    for uid, event in old_cache.items():
+        schedule_date = event.get('scheduled', None)
+        if (
+            schedule_date
+            and dt.datetime.strptime(schedule_date, '%Y-%m-%d %H:%M:%S%z').date()
+            >= cutoff_date
+        ):
+            old_events.append(uid)
+
+    new_cache = {}
+
+    if not delete_remote_events:
+        new_cache = old_cache
+
     with caldav.DAVClient(
         url=server_url,
         username=calendar['username'],
         password=calendar['password'],
     ) as client:
+        remote_calendar = client.calendar(url=calendar_url)
+
+        if not remote_calendar:
+            logging.error(f'Calendar not found: {calendar_id}')
+            return
+
         for i, event in enumerate(events):
-            remote_calendar = client.calendar(url=calendar_url)
-
-            if not remote_calendar:
-                logging.error(f'Calendar not found: {calendar_id}')
-                continue
-
             time_str = f'{event.scheduled:%Y-%m-%d %H:%M}'
             if event.scheduled and event.duration:
                 time_str += f'--{event.scheduled + event.duration:%H:%M}'
 
             status_string = f'[{i + 1:03}/{len(events):03}] [{time_str}] {event.title}'
+
+            if event.uid in old_events:
+                old_events.remove(event.uid)
+
+            new_cache[event.uid] = {
+                'title': event.title,
+                'scheduled': event.scheduled,
+                'duration': event.duration,
+                'description': event.description,
+            }
 
             if remote_event := Event.find_remote_event(remote_calendar, event.uid):
                 if event.compare_with_ical(remote_event):
@@ -62,8 +98,25 @@ def process_calendar(calendar: dict) -> None:
 
             logging.info(prefix + status_string)
 
+        if delete_remote_events:
+            for uid in old_events:
+                if remote_event := Event.find_remote_event(remote_calendar, uid):
+                    logging.info(f'Removing cached event "{old_cache[uid]["title"]}".')
+                    remote_event.delete()
 
-def main(config_file: str = 'config.yml', debug: bool = False):
+        write_cache_file(
+            cache_filename,
+            {
+                'cache': new_cache,
+            },
+        )
+
+
+def main(
+    config_file: str = 'config.yml',
+    debug: bool = False,
+    delete_remote_events: bool = False,
+) -> None:
     try:
         if not (config := read_config_file(config_file)):
             return
@@ -71,7 +124,7 @@ def main(config_file: str = 'config.yml', debug: bool = False):
         calendars = config.get('calendars', [])
 
         for calendar in calendars:
-            process_calendar(calendar)
+            process_calendar(calendar, delete_remote_events)
 
     except Exception as ex:
         if debug:
@@ -84,7 +137,8 @@ if __name__ == '__main__':
     try:
         args = parse_args()
         setup_logging(args.debug)
-        main(args.config)
+        check_cache_dir()
+        main(args.config, args.debug, args.delete_remote)
 
     except KeyboardInterrupt:
         print()
